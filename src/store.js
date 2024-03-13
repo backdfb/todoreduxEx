a90312870a899b2963ecb2b7dd18be7e8fd0bc75
backdfb <wva22lfr@gmail.com>
2024-03-13 16:10:14 +0900

    새로연습

---
+++ src/store.js
@@ -0,0 +1,18 @@
+import { legacy_createStore as createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+const reducer = combineReducers({
+
+});
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+    reducer,
+    initialState,
+    applyMiddleware(...middleware)
+); 
+
+export default store;
